e66e605477291df4208173a060a7b77313f7ce14
glullien <gurvan.lullien@gmail.com>
2017-02-02 17:14:37 +0100

    Add extract

---
+++ src/gcum/servlets/Extract.kt
@@ -0,0 +1,59 @@
+package gcum.servlets
+
+import gcum.db.Database
+import java.io.File
+import java.nio.file.Path
+import java.util.regex.Pattern
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+import javax.servlet.ServletException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
+@WebServlet(name = "Extract", value = "/extract")
+class Extract : HttpServlet() {
+
+   override fun doGet(request: HttpServletRequest, response: HttpServletResponse) {
+      request.characterEncoding = "UTF-8"
+      val districtParameter = request.getString("district", Pattern.compile("(all|\\d+)"))
+      val all = Database.photos
+      val photos = when (districtParameter) {
+         "all"->all
+         else-> {
+            val district = districtParameter.toInt()
+            all.filter {it.location.address.district == district}
+         }
+      }
+      val size = photos.map {it.file.length()}.sum()
+      response.contentType = "application/zip"
+      response.setContentLength(size.toInt())
+      ZipOutputStream(response.outputStream).use {
+         zip->
+         zip.setLevel(0)
+         val root = Database.root
+         val entryDirs = mutableListOf<Path>()
+         for (photo in photos.sortedBy {it.file.absolutePath}) {
+            fun putEntryDirs(path: Path) {
+               if (path.count() > 1) putEntryDirs(path.parent)
+               if (!entryDirs.contains(path)) {
+                  zip.putNextEntry(ZipEntry(path.joinToString("/") + "/"))
+                  entryDirs.add(path)
+               }
+            }
+
+            val fileName = photo.file
+            val pathFile = fileName.toPath()
+            if (!pathFile.startsWith(root)) throw ServletException("Wrong file name $pathFile - $root")
+            val entryPath = root.relativize(pathFile)
+            putEntryDirs(entryPath.parent)
+            val entry = ZipEntry(entryPath.joinToString("/"))
+            zip.putNextEntry(entry)
+            photo.file.inputStream().use {it.copyTo(zip)}
+            zip.closeEntry()
+         }
+      }
+   }
+}
---
+++ web/extract.jsp
@@ -0,0 +1,41 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>GCUM Extraits</title>
+    <link rel="stylesheet" type="text/css" href="stylesheets/shared.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/extract.css">
+    <script type="text/javascript" src="lib/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="scripts/extract.js"></script>
+</head>
+<body>
+<div id="controls">
+    <div class="links">
+        <a href="index.jsp">Carte</a>
+        <a href="add.jsp">Ajouter</a>
+    </div>
+</div>
+<div id="extractZone">
+    <a href="extract?district=all">Tout</a>
+    <a href="extract?district=1">1er</a>
+    <a href="extract?district=2">2nd</a>
+    <a href="extract?district=3">3e</a>
+    <a href="extract?district=4">4e</a>
+    <a href="extract?district=5">5e</a>
+    <a href="extract?district=6">6e</a>
+    <a href="extract?district=7">7e</a>
+    <a href="extract?district=8">8e</a>
+    <a href="extract?district=9">9e</a>
+    <a href="extract?district=10">10e</a>
+    <a href="extract?district=11">11e</a>
+    <a href="extract?district=12">12e</a>
+    <a href="extract?district=13">13e</a>
+    <a href="extract?district=14">14e</a>
+    <a href="extract?district=15">15e</a>
+    <a href="extract?district=16">16e</a>
+    <a href="extract?district=17">17e</a>
+    <a href="extract?district=18">18e</a>
+    <a href="extract?district=19">19e</a>
+    <a href="extract?district=20">20e</a>
+</div>
+</body>
+</html>
