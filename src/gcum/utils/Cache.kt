af9cd8c246e177fe50fc09b82e820ebeb65f6366
glullien <gurvan.lullien@gmail.com>
2017-02-26 15:53:00 +0100

    Remove warning

--- src/gcum/utils/Cache.kt
+++ src/gcum/utils/Cache.kt
@@ -4,5 +4,5 @@ import java.util.concurrent.ConcurrentHashMap
 
 class Cache<K, V> {
    private val cache = ConcurrentHashMap<K, V>()
-   fun get(k: K, v: () -> V) = cache.computeIfAbsent(k, {v()})
+   fun get(k: K, v: () -> V): V = cache.computeIfAbsent(k, {v()})
 }
