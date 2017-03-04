9c5a413f1671f9d3f4b35270be5482930d6ebac6
glullien <gurvan.lullien@gmail.com>
2017-03-04 08:18:42 +0100

    Twitter debug

--- src/gcum/conf/ReadConfiguration.kt
+++ src/gcum/conf/ReadConfiguration.kt
@@ -6,6 +6,7 @@ object Configuration {
    private val properties = KProperties(File(System.getProperty("user.home") + File.separator + ".gcum.conf"))
 
    fun getString(key: String) = properties.getString(key)
+   fun getLong(key: String) = properties.getLong(key)
    fun getBoolean(key: String) = properties.getBoolean(key)
    fun getPath(key: String) = properties.getPath(key)
 }
--- src/gcum/utils/Twitter.kt
+++ src/gcum/utils/Twitter.kt
@@ -3,16 +3,33 @@ package gcum.utils
 import gcum.conf.Configuration
 import gcum.db.Photo
 import twitter4j.StatusUpdate
+import twitter4j.Twitter
 import twitter4j.TwitterFactory
 import twitter4j.auth.AccessToken
+import twitter4j.conf.PropertyConfiguration
+import java.util.*
 import java.util.concurrent.Executors
 
 private class PhotoTwitter {
-   private val token = AccessToken(Configuration.getString("twitter_token"), Configuration.getString("twitter_token_secret"))
-   private val twitter = TwitterFactory().getInstance(token)
+   private val token: AccessToken = AccessToken(Configuration.getString("twitter.token"), Configuration.getString("twitter.tokenSecret"))
+   private val twitter: Twitter
+
+   init {
+      val properties = Properties(System.getProperties())
+      properties.setProperty("oauth.consumerKey", Configuration.getString("twitter.consumerKey"))
+      properties.setProperty("oauth.consumerSecret", Configuration.getString("twitter.consumerSecret"))
+      val configuration = PropertyConfiguration(properties)
+      twitter = TwitterFactory(configuration).getInstance(token)
+   }
+
    fun tweet(photo: Photo) {
       val statusUpdate = StatusUpdate(photo.location.address.text + " par " + photo.username + " #GCUM")
       statusUpdate.setMedia(photo.file)
+      twitter.updateStatus(statusUpdate)
+   }
+
+   fun tweetTest() {
+      val statusUpdate = StatusUpdate("coucou")
       twitter.updateStatus(statusUpdate)
    }
 }
@@ -27,4 +44,8 @@ fun tweet(photo: Photo) {
    photoTwitterExecutor?.execute {
       photoTwitter?.tweet(photo)
    }
+}
+
+fun main(args: Array<String>) {
+   photoTwitter?.tweetTest()
 }
